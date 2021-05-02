2827c26a09c7004c3c6f3f8962087df8413dfcc4
dazedmind <dazedmind1@protonmail.com>
2021-05-02 20:15:54 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    color: {
+      gray: {
+        dark: '#2e2e2e',
+      },
+    },
+    extend: {
+      fontFamily:{
+        custom:['Montserrat'],
+      },
+      colors:{
+        gray: {
+          light: '#333333',
+          dark: '#1c1c1c',
+          darker: '#161616',
+        },
+        teal: {
+          default: '#008080',
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
